e5a21d061aa5fbcee15f80fc609d8a79f01299b9
Anupam1707 <programmer.tiak@gmail.com>
2026-09-03 21:08:44 +0530

    feat: configure Firebase hosting and initialize project environment settings

--- public/config.js
+++ public/config.js
@@ -1,8 +1,9 @@
 window.__FIREBASE_CONFIG__ = {
-  apiKey: '',
-  authDomain: '',
-  projectId: 'landing-def',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: "AIzaSyAczhjLMigklGarXT43MKeIr36f9-iaL4M",
+  authDomain: "landing-def.firebaseapp.com",
+  projectId: "landing-def",
+  storageBucket: "landing-def.firebasestorage.app",
+  messagingSenderId: "1046202100326",
+  appId: "1:1046202100326:web:2a71871e25298824135ba7",
+  measurementId: "G-DGRRJDDGZE"
 };
